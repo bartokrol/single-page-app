675400b21c47c1710b37afaad2dab917395c5034
Bartosz Król <barto.kro@gmail.com>
2021-08-18 12:11:07 +0200

    create input for city name and create city state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-	const APIkey = "67fccf071e4c18dd1da570918ad48e4a";
-	const city = "Sopot";
-	const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
+	const [city, setCity] = useState("");
 	const [isFetch, setFetch] = useState(false);
 	const [apiData, setApiData] = useState("");
+	const APIkey = "67fccf071e4c18dd1da570918ad48e4a";
+	// const city = "Sopot";
+	const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
 
 	useEffect(() => {
-		if (isFetch) {
+		if (isFetch && city) {
 			fetch(api)
 				.then((response) => response.json())
 				.then((data) => {
@@ -18,15 +19,26 @@ function App() {
 				})
 				.catch((error) => console.log(error));
 		}
-	}, [apiData, isFetch, api]);
+	}, [apiData, isFetch, city, api]);
 
 	const handleFetchClick = () => {
 		const ifFetch = !isFetch;
 		setFetch(ifFetch);
 	};
 
+	const handleInputChange = (e) => {
+		const inputValue = e.target.value;
+		setCity(inputValue);
+	};
+
 	return (
 		<div className="App">
+			<input
+				type="text"
+				placeholder="Enter city name..."
+				onChange={handleInputChange}
+				value={city}
+			/>
 			<button onClick={handleFetchClick}>Klik</button>
 			<p>{apiData.name}</p>
 		</div>
