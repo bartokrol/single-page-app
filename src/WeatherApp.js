cd00f697abc7c02de9eae9eb4fb91ca267a9144b
Bartosz Król <barto.kro@gmail.com>
2021-09-02 11:40:13 +0200

    change App.js name into WeatherApp.js. Create styles folder and scss files

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,4 +1,4 @@
-import "./App.css";
+// import "./App.css";
 import { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import StartingPage from "./layout/StartingPage";
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+import WeatherApp from "./WeatherApp";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<WeatherApp />
 	</React.StrictMode>,
 	document.getElementById("root")
 );
